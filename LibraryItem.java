e60066427a8b05bdd2bd2333b5abbda46f02ebfc
Perception <cloudoutcast1@gmail.com>
2025-04-25 10:55:37 +0200

    changed string itemId to int itemId

--- LibraryItem.java
+++ LibraryItem.java
@@ -1,34 +1,34 @@
 public abstract class LibraryItem{
-    private String itemID;
+    private int itemID;
     private String title;
     private int publicationYear;
     private String publisher;
 
-    public LibraryItem(String itemID , String title , int publicationYear , String publisher){
+    public LibraryItem(int itemID , String title , int publicationYear , String publisher){
     this.itemID = itemID;
     this.title = title;
     this.publicationYear = publicationYear;
     this.publisher = publisher;
     }
 
-public String getitemID(){
-return itemID;
-}
-
-public void setitemID(String itemID){
-this.itemID = itemID;
-}
-
-public String gettitle(){
-    return title;
+    public int getitemID(){
+        return itemID;
     }
+
+    public void setitemID(int itemID){
+        this.itemID = itemID;
+        }
+
+        public String gettitle(){
+            return title;
+        }
     
-    public void settitle(String title){
-    this.title = title;
-    }
+            public void settitle(String title){
+            this.title = title;
+        }
 
-    public int getpublicationYear(){
-        return publicationYear;
+                public int getpublicationYear(){
+                return publicationYear;
         }
         
         public void setpublicationYear(int publicationYear){
