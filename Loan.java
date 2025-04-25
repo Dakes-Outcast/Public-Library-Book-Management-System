84bd3b63d2da6646570ad74e1901effcb8530840
Letso Maitshwenyo <letsomaitshwenyo@gmail.com>
2025-04-25 20:06:19 +0200

    Created a loan class using book as a composed field

---
+++ Loan.java
@@ -0,0 +1,44 @@
+
+       public class Loan {
+        private Book book; // Composed Book object
+        private String dueDate;
+        
+    
+        // Constructor with Book composition
+        public Loan(Book book, String dueDate) {
+            this.book = book;
+            this.dueDate = dueDate;
+            
+        }
+    
+        // Getters and Setters
+        public Book getBook() {
+            return book;
+        }
+    
+        public void setBook(Book book) {
+            this.book = book;
+        }
+    
+
+    
+        public String getDueDate() {
+            return dueDate;
+        }
+    
+        public void setDueDate(String dueDate) {
+            this.dueDate = dueDate;
+        }
+    
+        
+        // Method to mark the book as returned
+        
+    
+        @Override
+        public String toString() {
+            return "Loan Details:\n" +
+                   "Book: " + book.getTitle() + " by " + book.getAuthor() + "\n" +
+                   "Due Date: " + dueDate + "\n" +
+                  
+        }
+    }
