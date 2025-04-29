6ce11889f3d6645a1c249dd616ab709d5cf4de85
Mokgolele Mokolele <mokgolelemokgolele@gmail.com>
2025-04-29 16:16:36 +0200

    Added necessary error messages on methods

--- Library.java
+++ Library.java
@@ -50,18 +50,26 @@ public class Library
 
     public void searchBooks(String searchTerm)
     {
-        java.util.ArrayList<LibraryItem> titleresults = utility.titleSearch(searchTerm);
-        if (titleresults.isEmpty()) 
+        try
         {
-           throw new ItemNotFoundException("No books found with the title: " + searchTerm);
+             java.util.ArrayList<LibraryItem> titleresults = utility.titleSearch(searchTerm);
+         if (titleresults.isEmpty()) 
+         {
+            throw new ItemNotFoundException("No books found with the title: " + searchTerm);
     
-        }  
-        {
+         }  
+         {
             System.out.println("Books found with the title: " + searchTerm);
             for (LibraryItem item : titleresults) 
             {
                 System.out.println(item.getItemDetails());
             }
+         }
+        }
+        catch(ItemNotFoundException e)
+        {
+            System.out.println(e.getMessage);
+
         }
 
     }
@@ -70,5 +78,68 @@ public class Library
         Book book = (Book) item;
         System.out.println("Book found with ID: ");
         System.out.println(book.getItemDetails());
+    }
+
+
+    public void loanBook(int bookID, String dueDate)
+    {
+        try
+        {
+            LibraryItem item = utility.findItemByID(bookID);
+
+            if (!(Item instanceof Book))
+            {
+                System.out.printin("Book with ID " + bookID + "is not a book");
+                return;
+            }
+
+            Book book = (Book) item;
+            if(book.isLaoned)
+            {
+                System.out.println("Book wih ID " + bookID +  " is already on loan");
+                return;
+            }
+
+            Loan loan = new Loan(book, dueDate);
+            utility.addLoan(loan);
+
+            System.out.println("Book successfully loaned");
+            System.out.println(loan);
+        }
+        catch(ItemNotFoundException e)
+        {
+            System.out.println(e.getMessage);
+        }
+    }
+
+    public void returnBook(int bookID)
+    {
+        try
+        {
+            utility.returnBook(bookID);
+            System.out.println("Book with ID " + bookID + "has been returned successfully");
+        }
+        catch (ItemNotFoundException e)
+        {
+            System.out.println(e.getMessage);
+        }
+    }
+
+    public void findBookByID(int bookID)
+    {
+        try
+        {
+            LibraryItem item = utility.findItemByID(bookID);
+
+            if(!(item instanceof Book))
+            {
+                System.out.println("Item with ID: " + bookID + "is not a book");
+                return;
+            }
+
+            Book book = (Book) item;
+            System.out.println("Book found :")
+            System.out.println(e.getMessage)
+        }
     }
 }
