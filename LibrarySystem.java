c34f0b9287ab29036656797bac34cf6a2a0a6ead
Perception <cloudoutcast1@gmail.com>
2025-04-30 12:15:50 +0200

    edited the layout of the System

--- LibrarySystem.java
+++ LibrarySystem.java
@@ -37,9 +37,9 @@ public class LibrarySystem{
         StartingLibrary();
         
         // Display welcome message
-        System.out.println("========================================");
-        System.out.println(" WELCOME TO LIBRARY MANAGEMENT SYSTEM ");
-        System.out.println("========================================");
+        System.out.println("\t\t\t========================================\t\t\t");
+        System.out.println(" \t\t\tWELCOME TO LIBRARY MANAGEMENT SYSTEM\t\t\t");
+        System.out.println("\t\t\t========================================\t\t\t");
         
         // Start the main menu
         mainMenu();
@@ -70,11 +70,11 @@ public class LibrarySystem{
         boolean exit = false;
         
         while (!exit) {
-            System.out.println("\n===== MAIN MENU =====");
-            System.out.println("1. Manager Mode");
-            System.out.println("2. User Mode");
-            System.out.println("0. Exit");
-            System.out.print("Enter your choice: ");
+            System.out.println("\n\t===== MAIN MENU =====\t");
+            System.out.println("\t1. Manager Mode\t");
+            System.out.println("\t2. User Mode\t");
+            System.out.println("\t0. Exit\t");
+            System.out.print("\tEnter your choice:  \t");
             
             int choice = Intnum(); // Get user choice
             
@@ -114,7 +114,7 @@ public class LibrarySystem{
             System.out.println("5. Search books by title");
             System.out.println("6. Find book by ID");
             System.out.println("0. Return to main menu");
-            System.out.print("Enter your choice: ");
+            System.out.print("Enter your choice: \n");
             
             int choice = Intnum(); // Get user choice
             
@@ -165,7 +165,7 @@ public class LibrarySystem{
             System.out.println("4. Borrow a book");
             System.out.println("5. Return a book");
             System.out.println("0. Return to main menu");
-            System.out.print("Enter your choice: ");
+            System.out.print("Enter your choice: \n");
             
             int choice = Intnum(); // Get user choice
             
